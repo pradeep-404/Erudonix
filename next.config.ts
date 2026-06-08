b41283d6f8650087f4dc9ad09c70129c4a6f6557
Pradeep_Choudhary <pradeep_choudhary@Pradeep-Choudharys-MacBook-Air.local>
2026-06-08 20:56:11 +0530

    chore: inject AWS environment variables at build-time in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    RDSHOST: process.env.RDSHOST || '',
+    USE_RDS: process.env.USE_RDS || '',
+    DB_NAME: process.env.DB_NAME || '',
+    DB_USER: process.env.DB_USER || '',
+  }
 };
 
 export default nextConfig;
